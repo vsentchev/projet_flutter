f85f8a07def31b6ad3eb05757856d554f83c3505
v.sentchev <120784022+vsentchev@users.noreply.github.com>
2023-03-29 19:39:38 +0200

    bdd test avec mini interface
    je peux scroll pour ajouter des mots avec un user , et clean la bdd

---
+++ bdd_vass/lib/db_helper.dart
@@ -0,0 +1,61 @@
+import 'package:sqflite/sqflite.dart';
+
+class DbHelper {
+  static const String _dbName = 'mywords.db';
+  static const int _dbVersion = 1;
+  static const String _tableName = 'words';
+
+  static Database? _db;
+
+  static initDb() async {
+    final String dbPath = await getDatabasesPath();
+    final String path = dbPath + _dbName;
+    final Database database = await openDatabase(
+      path,
+      version: _dbVersion,
+      onCreate: _onCreate,
+      onUpgrade: _onUpgrade,
+    );
+    _db = database;
+  }
+
+  static const String _createTable = '''
+    CREATE TABLE IF NOT EXISTS $_tableName (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      word TEXT NOT NULL
+    )
+  ''';
+
+  static const String _dropTable = '''
+    DROP TABLE IF EXISTS $_tableName
+  ''';
+
+  static _onCreate(Database db, int version) async {
+    await db.execute(_createTable);
+    //await insertWord('houssam', 'vassili');
+  }
+
+  static _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    await db.execute(_dropTable);
+    await _onCreate(db, newVersion);
+  }
+
+  static Future<int> insertWord(String username, String word) async {
+    final Map<String, dynamic> row = {
+      'username': username,
+      'word': word,
+    };
+    final int id = await _db!.insert(_tableName, row);
+    return id;
+  }
+
+  static Future<void> deleteWords() async {
+    await _db!.delete(_tableName);
+  }
+
+  static Future<List<Map<String, dynamic>>> getWords() async {
+    final List<Map<String, dynamic>> rows = await _db!.query(_tableName);
+    return rows;
+  }
+}
---
+++ bdd_vass/lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/db_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/db_helper.dart';
+
+void main() async {
+  // Initialise la base de données
+  WidgetsFlutterBinding.ensureInitialized();
+  await DbHelper.initDb();
+
+  // Insère des données dans la base de données
+  await DbHelper.insertWord('Houssam', 'mignon');
+  await DbHelper.insertWord('Vassili', 'relou');
+
+  // Récupère les données de la base de données
+  final words = await DbHelper.getWords();
+
+  // Affiche les données à l'écran
+  runApp(
+    MyApp(words: words),
+  );
+}
+
+/*La classe MyApp hérite de la classe StatelessWidget. Elle sert à définir le thème de l'application et à définir la page d'accueil de l'application en appelant la classe MyHomePage avec les données récupérées de la base de données. */
+class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> words;
+
+  const MyApp({Key? key, required this.words}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(words: words),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final List<Map<String, dynamic>> words;
+
+  const MyHomePage({Key? key, required this.words}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+/*La classe MyHomePage hérite de la classe StatefulWidget. Elle contient l'état de la page d'accueil de l'application, qui peut être modifié à tout moment. Elle reçoit en paramètre les données récupérées de la base de données. */
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _wordController = TextEditingController();
+  List<Map<String, dynamic>> _words = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _words = widget.words;
+  }
+
+  // Fonction pour récupérer les données de la base de données et les afficher dans l'interface
+  Future<void> _getWords() async {
+    final words = await DbHelper.getWords();
+    setState(() {
+      _words = words;
+    });
+  }
+
+  // Fonction pour insérer un nouveau mot dans la base de données
+  Future<void> _insertWord() async {
+    final String username = _usernameController.text;
+    final String word = _wordController.text;
+    await DbHelper.insertWord(username, word);
+    _usernameController.clear();
+    _wordController.clear();
+    _getWords();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BDD test'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(hintText: 'Username'),
+                ),
+                TextField(
+                  controller: _wordController,
+                  decoration: InputDecoration(hintText: 'Little'),
+                ),
+                ElevatedButton(
+                  onPressed: _insertWord,
+                  child: Text('Insert'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await DbHelper.deleteWords();
+                    _getWords();
+                  },
+                  child: Text('Delete all words'),
+                ),
+              ],
+            ),
+          ),
+          // La liste des mots récupérés depuis la base de données et affichés dans l'interface
+          Expanded(
+            child: ListView.builder(
+              itemCount: _words.length,
+              itemBuilder: (BuildContext context, int index) {
+                final word = _words[index];
+                return ListTile(
+                  title: Text(word['word']),
+                  subtitle: Text(word['username']),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
